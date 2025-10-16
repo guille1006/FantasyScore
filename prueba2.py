c4140c9bf6bff2743fee27360fae0ee45c590b46
guille1006 <guilleia1006@gmail.com>
2025-10-17 00:22:12 +0200

    explicacion de todas las variables de los jugadreso

--- prueba2.py
+++ prueba2.py
@@ -1,21 +1,67 @@
-from utils.botasaurus_getters import botasaurus_browser_get_json, botasaurus_request_get_json
-from utils.load_comps import get_module_comps
-
-API_PREFIX = 'https://api.sofascore.com/api/v1'
-comps = get_module_comps("SOFASCORE")
-league = "Spain La Liga"
+ee = [
+ 'accurateKeeperSweeper',
 
 
-url = f'{API_PREFIX}/unique-tournament/{comps[league]["SOFASCORE"]}/seasons/'
-response = botasaurus_browser_get_json(url)
-matches = list()
-i = 0
-while 1:
-    response = botasaurus_browser_get_json(
-        f'{API_PREFIX}/unique-tournament/{comps[league]["SOFASCORE"]}/' +
-        f'season/{valid_seasons[year]}/events/last/{i}'
-    )
-    if 'events' not in response:
-        break
-    matches += response['events']
-    i += 1
+
+
+ 'aerialLost',
+ 'aerialWon',
+ 'ballRecovery',
+ 'bigChanceCreated',
+
+
+ 'challengeLost',
+ 'clearanceOffLine',
+ 'crossNotClaimed',
+ 'dispossessed',
+ 'duelLost',
+
+ 'errorLeadToAGoal',
+ 'errorLeadToAShot',
+
+
+
+ 'fouls',
+ 'goalAssist',
+
+ 'goalsPrevented',
+ 'goodHighClaim',
+
+ 'interceptionWon',
+
+ 'lastManTackle',
+ 'minutesPlayed',
+
+ 'outfielderBlock',
+ 'ownGoals',
+ 'penaltyConceded',
+ 'penaltyFaced',
+ 'penaltyMiss',
+ 'penaltySave',
+ 'penaltyWon',
+
+ 'punches',
+
+
+
+ 'savedShotsFromInsideTheBox',
+ 'saves',
+
+ 'totalClearance',
+ 'totalContest',
+
+ 'totalKeeperSweeper',
+
+
+
+
+
+
+ 'totalTackle',
+
+ 'unsuccessfulTouch',
+ 'wasFouled',
+ 'wonContest',
+ 'wonTackle']
+
+print(len(ee))
